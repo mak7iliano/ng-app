3cb6ae800df18a2fdf952fe1b436764168463b19
mbondaruk <mak7iliano@gmail.com>
2018-03-19 09:52:27 +0200

    team view change

--- js/controllers/teamCtrl.js
+++ js/controllers/teamCtrl.js
@@ -2,6 +2,15 @@ app.controller('teamCtrl', function($scope, teamFactory){
     $scope.title = "Team";
     $scope.setActive("team");
     $scope.loading = true;
+    $scope.isList = true;
+
+    $scope.viewType = function(type) {
+        if (type === 'list') {
+            $scope.isList = true;
+        } else {
+            $scope.isList = false;
+        }
+    }
 
     teamFactory.getTeam().then(function(data){
         $scope.team = data;
